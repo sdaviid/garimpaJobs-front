187dc8bcf98b863bbfb7d1bbf3fd7506eb0a13c0
David Souza <sdaviid@gmail.com>
2021-10-18 16:51:16 -0300

    1810

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from config import Config
 
 
-API_END_POINT = 'lalalalla'
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
 
-# Load the views
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    return app
+
+
+
+
+app = create_app()
+
+
 from app import views
 
-# Load the config file
-app.config.from_object('config')
--- config.py
+++ config.py
@@ -1,2 +1,17 @@
-DEBUG = True
-API_END_POINT = 'http://192.168.1.103:8000/'
+
+class Config:
+    FLASK_ENV='development'
+    ENV='development'
+    DEBUG = 1
+    FLASK_DEBUG = 1
+    API_END_POINT = 'http://192.168.1.103:8000/'
+    HOST = '0.0.0.0'
+    PORT = 5000
+
+
+
+
+# FLASK_ENV='development'
+# ENV='development'
+# DEBUG = True
+# API_END_POINT = 'http://192.168.1.103:8000/'
